5eedb2e82322e30039c087f6c2fbe201a7421452
Alex Pavlov <al.an.pavlov@gmail.com>
2024-01-29 22:22:05 +0200

    add backup folder + first script

---
+++ backup/vertica_bup_s3.sh
@@ -0,0 +1,7 @@
+docker exec -it vertica-ce bash
+opt/vertica/bin/vbr --task init -c backup_test/db_backup.ini
+
+export VBR_BACKUP_STORAGE_ACCESS_KEY_ID=<key>
+export VBR_BACKUP_STORAGE_SECRET_ACCESS_KEY=<secret-key>
+
+opt/vertica/bin/vbr --task backup -c backup_test/db_backup.ini
