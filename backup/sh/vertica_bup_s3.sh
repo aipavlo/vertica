664470eeac8d15ddb9eae5d78cbc5b7ff3cf3719
Alex Pavlov <122205929+aipavlo@users.noreply.github.com>
2024-02-05 12:57:05 +0300

    Update vertica_bup_s3.sh

--- backup/sh/vertica_bup_s3.sh
+++ backup/sh/vertica_bup_s3.sh
@@ -1,5 +1,5 @@
 docker exec -it vertica-ce bash
-opt/vertica/bin/vbr --task init -c backup_test/db_backup.ini
+opt/vertica/bin/vbr --task init -c backup_configs/db_backup.ini
 
 export VBR_BACKUP_STORAGE_ACCESS_KEY_ID=<key>
 export VBR_BACKUP_STORAGE_SECRET_ACCESS_KEY=<secret-key>
