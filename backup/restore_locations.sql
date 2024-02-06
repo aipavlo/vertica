90144ac1d2e77738bd661ca1b555af2de4a8059a
Alex Pavlov <122205929+aipavlo@users.noreply.github.com>
2024-02-06 15:41:58 +0300

    Delete backup/restore_locations.sql

--- backup/restore_locations.sql
+++
@@ -1,7 +0,0 @@
-SELECT 'SELECT ADD_LOCATION(' || COALESCE(location_path,'') || ', ' || 
-COALESCE(node_name,'') || ', ' || 
-COALESCE(location_usage ,'') || ', ' || 
-COALESCE(location_label  ,'') || ');'
-FROM storage_locations;
-
-SELECT ADD_LOCATION(s3://vertica-ext-tables/ext_etl/, , USER, s3_ext_etl);
