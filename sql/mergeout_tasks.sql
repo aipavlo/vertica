2b0de98b55c56cdd7dfe5e2aef93d14055366198
Alex Pavlov <122205929+aipavlo@users.noreply.github.com>
2024-03-06 10:47:21 +0300

    Update mergeout_tasks.sql

--- sql/mergeout_tasks.sql
+++ sql/mergeout_tasks.sql
@@ -3,3 +3,5 @@ SELECT node_name, schema_name, table_name, request_type, event_type
 FROM mergeout_profiles 
 WHERE event_type='REQUEST_QUEUED';
 
+-- create tuple mover task
+SELECT DO_TM_TASK('mergeout', 'schema_name.table_name');
