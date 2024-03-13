146a898e61d8a752f0feda37b5e7d4b85cf633d7
Alex Pavlov <122205929+aipavlo@users.noreply.github.com>
2024-03-13 14:44:01 +0300

    Update vertica_bup_s3.sh

--- backup/sh/vertica_bup_s3.sh
+++ backup/sh/vertica_bup_s3.sh
@@ -1,5 +1,6 @@
 docker exec -it vertica-ce bash
 opt/vertica/bin/vbr --task init -c backup_configs/db_backup.ini
+# run init task with --s3-force-init/--cloud-force-init flag to regenerate the identity files
 
 export VBR_BACKUP_STORAGE_ACCESS_KEY_ID=<key>
 export VBR_BACKUP_STORAGE_SECRET_ACCESS_KEY=<secret-key>
